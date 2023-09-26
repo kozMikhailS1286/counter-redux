ff2e6e9a5ef2cc6f7179865abe8efbcc927c775a
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-09-26 16:43:07 +0300

    Commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import React from 'react';
 import './App.css';
 import {AppStateType} from "./state/store";
+import {incValueAC} from "./state/counterReducer";
 
 function App() {
+
+    const dispatch = useDispatch()
 
     const value = useSelector<AppStateType, number>(state => state.counter.value)
 
 const incHandler = () => {
-
+    dispatch(incValueAC())
 }
     return (
         <div className="App">
--- src/state/counterReducer.ts
+++ src/state/counterReducer.ts
@@ -2,7 +2,32 @@ const initialState = {
     value: 0
 }
 
+
+
 type initialStateType = typeof initialState;
-export const counterReducer = (state: initialStateType = initialState, action: any): initialStateType => {
-    return state;
+export const counterReducer = (state: initialStateType = initialState, action: ActionType): initialStateType => {
+    switch (action.type) {
+        case 'INC-VALUE':
+            return {
+                ...state,
+                value: state.value + 1
+            }
+        case 'INC-VALUE-FROM-LOCAl-STORAGE': {
+            return {
+                ...state,
+                value: action.value
+            }
+        }
+        default:
+            return state;
+    }
 }
+
+export const incValueAC = () => ({type: 'INC-VALUE'} as const)
+export const incValueFromLocalStorageAC = (value: number) => ({type: 'INC-VALUE-FROM-LOCAl-STORAGE', value} as const)
+
+
+export type incActionType = ReturnType<typeof incValueAC>
+export type incActionFromLocalStorageType = ReturnType<typeof incValueFromLocalStorageAC>
+
+type ActionType = incActionType | incActionFromLocalStorageType
