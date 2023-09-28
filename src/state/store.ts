d472b465e901cddef80d858630a7175969174490
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-09-28 17:49:11 +0300

    Finish

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,25 +2,27 @@ import {createStore, combineReducers, applyMiddleware, AnyAction} from 'redux';
 import {counterReducer} from "./counterReducer";
 import thunk, {ThunkDispatch} from 'redux-thunk'
 import {useDispatch} from "react-redux";
+import {loadState, saveState} from "../utils/localstoraje-utils";
+import app from "../App";
 
 
 const rootReducer = combineReducers({
     counter: counterReducer
 })
 
-let preloadedState;
-const persistedTodosString = localStorage.getItem('app-state')
-if (persistedTodosString) {
-    preloadedState = JSON.parse(persistedTodosString)
-}
 
-export const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+
+
+
+
+export const store = createStore(rootReducer, loadState(), applyMiddleware(thunk));
 
 export const useAppDispatch = () => useDispatch<ThunkDispatch<AppStateType, any, AnyAction>>()
 
 store.subscribe(() => {
-    localStorage.setItem('app-state', JSON.stringify(store.getState()))
-    localStorage.setItem('value', JSON.stringify(store.getState().counter.value))
+    saveState({
+        counter: store.getState().counter
+    })
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
