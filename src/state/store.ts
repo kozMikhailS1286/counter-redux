66cc80929576414fa4c5f4d1d01c33699c08b2db
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-09-28 17:33:51 +0300

    Commit 5

--- src/state/store.ts
+++ src/state/store.ts
@@ -8,14 +8,20 @@ const rootReducer = combineReducers({
     counter: counterReducer
 })
 
-export type AppStateType = ReturnType<typeof rootReducer>
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+let preloadedState;
+const persistedTodosString = localStorage.getItem('app-state')
+if (persistedTodosString) {
+    preloadedState = JSON.parse(persistedTodosString)
+}
+
+export const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 
 export const useAppDispatch = () => useDispatch<ThunkDispatch<AppStateType, any, AnyAction>>()
 
 store.subscribe(() => {
     localStorage.setItem('app-state', JSON.stringify(store.getState()))
+    localStorage.setItem('value', JSON.stringify(store.getState().counter.value))
 })
 
-
+export type AppStateType = ReturnType<typeof rootReducer>
 export type AppStoreType = typeof store
